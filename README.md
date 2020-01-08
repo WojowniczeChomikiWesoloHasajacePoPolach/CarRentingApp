134fbe8ee977532da061dc8bd4cc78c11e544404
Fotsik <fotskontakt@gmail.com>
2020-01-08 23:13:58 +0100

    readme fixes

--- README.md
+++ README.md
@@ -10,16 +10,16 @@ It is an app created with Node.js, React and MongoDB.
 This is a training project for Coders Camp 5th edition - it was an opportunity for us to get familiar with all technologies mentioned below. The app allows registered users to rent cars from database, which is created by admins.
 
 ## Technologies
-* Node.js 12.13.0
-* React 16.12.0
-* React-DOM 16.12.0
-* Redux 4.0.5
-* MongoDB, 
-* Mongoose 5.7.13
+* [Node.js 12.13.0](https://nodejs.org/en/)
+* [React 16.12.0](https://reactjs.org/)
+* [React-DOM 16.12.0](https://www.npmjs.com/package/react-dom)
+* [Redux 4.0.5](https://redux.js.org/)
+* [MongoDB](https://www.mongodb.com/) 
+* [Mongoose 5.7.13](https://mongoosejs.com/) 
 
 ## Launch
 To launch it You need to open bash: 
 * In /src/back-end type:
-$ node main.js
+`$ node main.js`
 * In /src/client type: 
-$ npm start
+`$ npm start`
--- src/client/src/Components/OrderForm.js
+++ src/client/src/Components/OrderForm.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import Select from './Select';
-import { connect } from 'react-redux'
 import { getCars } from '../actions';
+import { connect } from 'react-redux';
 
 
 
 class OrderForm extends React.Component{ 
-
     constructor(props) {
         super(props);
          this.state = {
@@ -19,9 +18,22 @@ class OrderForm extends React.Component{
     componentDidMount() {
         this.props.getCars();
         console.log(this.props.car)
-
     }
 
+    // test(){
+    //     return this.props.car.map( car => {
+    //         let rented = "No";
+    //         if (car.isRent === true) { rented = "Yes"};
+    //         return (
+    //             <tr>
+    //                 <td data-label="Car">{car.brand} {car.model}</td>
+    //                 <td data-label="Registry Number">{car.registryNumber}</td>
+    //                 <td data-label="Rented">{rented}</td>
+    //                 <td data-label=''><Link to={`/admin/cars/${car._id}`} > <button className="ui button">EDIT</button></Link></td>
+
+    //               </tr>)
+    // });
+    // }
     
 
     render(){
@@ -30,7 +42,7 @@ class OrderForm extends React.Component{
                 <h2>Find your car!</h2>
                     <div>
                         <p>Brand:</p>
-                        <Select content={this.state.car.brand}/>
+                        <Select content={this.state.brand}/>
                     </div>
                     <div>
                         <p>Model:</p>
